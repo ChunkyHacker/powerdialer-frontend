caaaa0f59078752f6783da06b530e28ada0c8630
ChunkyHacker <chunkydaddy10@gmail.com>
2026-07-22 06:41:28 +0800

    Add temporary protected routing

--- src/app/router/ProtectedRoute.jsx
+++ src/app/router/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router'
 
 function ProtectedRoute() {
   // TODO: Replace this temporary value with backend or auth-context state.
-  const isAuthenticated = false
+  const isAuthenticated = true
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
 }
